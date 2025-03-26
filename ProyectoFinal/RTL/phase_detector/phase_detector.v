0f2ff5e362bf18636d88f8870ecdc46a65923fea
Ignaciob07 <ignacio.obalbo@gmail.com>
2025-03-26 08:42:28 -0300

    working on phase detector

--- ProyectoFinal/RTL/phase_detector/phase_detector.v
+++ ProyectoFinal/RTL/phase_detector/phase_detector.v
@@ -1,12 +1,36 @@
 module phase_detector #(
-    parameter NB_IN     = 8     ,
-    parameter NB_OUT    = 16
+    parameter NB_IN_PD   = 8     ,
+    parameter NB_OUT_PD  = 16
 ) (
-    output  signed  [NB_OUT - 1 : 0]     o_phase_error  ,
-    input   signed  [NB_IN  - 1 : 0]     i_in_phase     ,
-    input   signed  [NB_IN  - 1 : 0]     i_quadrature   
+    output  signed  [NB_OUT_PD - 1 : 0]     o_phase_error  ,
+    input   signed  [NB_IN_PD  - 1 : 0]     i_in_phase     ,
+    input   signed  [NB_IN_PD  - 1 : 0]     i_quadrature   ,
+    input                                   i_clock        ,
+    input                                   i_rst_n
 );
 
+lut_atan #(
+    .NB_DATA_IN     (NB_IN_PD       ),
+    .NB_DATA_OUT    (NB_OUT_PD      )
+) u_lut_atan_r ( // r: recived symbol
+    .o_atan         (o_atan         ),
+    .i_data_i       (i_data_i       ),
+    .i_data_q       (i_data_q       ),
+    .i_clock        (i_clock        ),
+    .i_rst_n        (i_rst_n        )    
+);
 
+lut_atan #(
+    .NB_DATA_IN     (NB_IN_PD       ),
+    .NB_DATA_OUT    (NB_OUT_PD      )
+) u_lut_atan_a ( // a: decided symbol
+    .o_atan         (o_atan         ),
+    .i_data_i       (i_data_i       ),
+    .i_data_q       (i_data_q       ),
+    .i_clock        (i_clock        ),
+    .i_rst_n        (i_rst_n        )    
+);
+
+    assign o_phase_error
     
 endmodule
